ea51de350bda6e054126443911bda474c78a23a5
Maia Dandachi <37670647+MaiaDandachi@users.noreply.github.com>
2019-10-03 12:40:13 +0300

    Add files via upload

---
+++ candies.py
@@ -0,0 +1,32 @@
+def candies(n,arr):
+
+    candies = [1] * (len(arr))
+
+
+    # iterate from left to right from 1 to end the end of the array
+    for i in range(1, n):
+        if arr[i] > arr[i - 1]:
+            candies[i] = candies[i - 1] + 1
+
+    for j in range(n-2, -1, -1):
+        if arr[j] > arr[j + 1] and candies[j] < candies[j + 1] + 1:
+            candies[j] = candies[j + 1] + 1
+
+    total_candies = sum(candies)
+    return  total_candies
+
+
+
+
+
+
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    student_rate = int(input())
+    arr.append(student_rate)
+
+print(candies(n, arr))
+
